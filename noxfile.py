f32549da76a0ac80580f17f77d7b654763c60501
Tobias Fehlmann <tobias@astera.org>
2022-09-12 20:37:57 -0700

    Add more github workflows from vscode-pylint template

--- noxfile.py
+++ noxfile.py
@@ -5,6 +5,7 @@
 import json
 import os
 import pathlib
+import re
 import urllib.request as url_lib
 from typing import List
 
@@ -85,24 +86,27 @@ def _update_npm_packages(session: nox.Session) -> None:
     if not new_package_json.endswith("\n"):
         new_package_json += "\n"
     package_json_path.write_text(new_package_json, encoding="utf-8")
+
+    session.run("npm", "audit", "fix", external=True)
     session.run("npm", "install", external=True)
 
 
 def _setup_template_environment(session: nox.Session) -> None:
     session.install("wheel", "pip-tools")
-    session.run("pip-compile", "--generate-hashes", "--upgrade", "./requirements.in")
-    session.run(
-        "pip-compile",
-        "--generate-hashes",
-        "--upgrade",
-        "./src/test/python_tests/requirements.in",
-    )
+    _update_pip_packages(session)
     _install_bundle(session)
 
 
-@nox.session()
+@nox.session(python="3.7")
+def install_bundled_libs(session):
+    """Installs the libraries that will be bundled with the extension."""
+    session.install("wheel")
+    _install_bundle(session)
+
+
+@nox.session(python="3.7")
 def setup(session: nox.Session) -> None:
-    """Sets up the template for development."""
+    """Sets up the extension for development."""
     _setup_template_environment(session)
 
 
@@ -120,16 +124,13 @@ def lint(session: nox.Session) -> None:
     session.install("-r", "src/test/python_tests/requirements.txt")
 
     session.install("pylint")
-    session.run("pylint", "-d", "W0511", "./bundled/tool")
+    session.run("pylint", "./bundled/tool")
     session.run(
         "pylint",
-        "-d",
-        "W0511",
         "--ignore=./src/test/python_tests/test_data",
         "./src/test/python_tests",
     )
-    session.run("pylint", "-d", "W0511", "noxfile.py")
-
+    session.run("pylint", "noxfile.py")
     # check formatting using black
     session.install("black")
     session.run("black", "--check", "./bundled/tool")
@@ -156,8 +157,70 @@ def build_package(session: nox.Session) -> None:
 
 
 @nox.session()
+def update_build_number(session: nox.Session) -> None:
+    """Updates build number for the extension."""
+    if len(session.posargs) == 0:
+        session.log("No updates to package version")
+        return
+
+    package_json_path = pathlib.Path(__file__).parent / "package.json"
+    session.log(f"Reading package.json at: {package_json_path}")
+
+    package_json = json.loads(package_json_path.read_text(encoding="utf-8"))
+
+    parts = re.split("\\.|-", package_json["version"])
+    major, minor = parts[:2]
+
+    version = f"{major}.{minor}.{session.posargs[0]}"
+    version = version if len(parts) == 3 else f"{version}-{''.join(parts[3:])}"
+
+    session.log(f"Updating version from {package_json['version']} to {version}")
+    package_json["version"] = version
+    package_json_path.write_text(json.dumps(package_json, indent=4), encoding="utf-8")
+
+
+def _get_module_name() -> str:
+    package_json_path = pathlib.Path(__file__).parent / "package.json"
+    package_json = json.loads(package_json_path.read_text(encoding="utf-8"))
+    return package_json["serverInfo"]["module"]
+
+
+@nox.session()
+def validate_readme(session: nox.Session) -> None:
+    """Ensures the linter version in 'requirements.txt' matches 'readme.md'."""
+    requirements_file = pathlib.Path(__file__).parent / "requirements.txt"
+    readme_file = pathlib.Path(__file__).parent / "README.md"
+
+    lines = requirements_file.read_text(encoding="utf-8").splitlines(keepends=False)
+    module = _get_module_name()
+    formatter_ver = list(line for line in lines if line.startswith(module))[0]
+    name, version = formatter_ver.split(" ")[0].split("==")
+
+    session.log(f"Looking for {name}={version} in README.md")
+    content = readme_file.read_text(encoding="utf-8")
+    if f"{name}={version}" not in content:
+        raise ValueError(f"Linter info {name}={version} was not found in README.md.")
+    session.log(f"FOUND {name}={version} in README.md")
+
+
+def _update_readme() -> None:
+    requirements_file = pathlib.Path(__file__).parent / "requirements.txt"
+    lines = requirements_file.read_text(encoding="utf-8").splitlines(keepends=False)
+    module = _get_module_name()
+    formatter_ver = list(line for line in lines if line.startswith(module))[0]
+    _, version = formatter_ver.split(" ")[0].split("==")
+
+    readme_file = pathlib.Path(__file__).parent / "README.md"
+    content = readme_file.read_text(encoding="utf-8")
+    regex = r"\`([a-zA-Z0-9]+)=([0-9]+\.[0-9]+\.[0-9]+)\`"
+    result = re.sub(regex, f"`{module}={version}`", content, 0, re.MULTILINE)
+    content = readme_file.write_text(result, encoding="utf-8")
+
+
+@nox.session()
 def update_packages(session: nox.Session) -> None:
     """Update pip and npm packages."""
     session.install("wheel", "pip-tools")
     _update_pip_packages(session)
     _update_npm_packages(session)
+    _update_readme()
